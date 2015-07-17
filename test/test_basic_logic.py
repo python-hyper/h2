b527a6f84679919ab5e2c526d43db77af1c161b5
Cory Benfield <lukasaoz@gmail.com>
2015-07-17 17:14:39 +0100

    Basic sending data test

--- test/test_basic_logic.py
+++ test/test_basic_logic.py
@@ -23,3 +23,9 @@ class TestBasicConnection(object):
         c = h2.connection.H2Connection()
         frames = c.send_headers_on_stream(1, self.example_request_headers)
         assert len(frames) == 1
+
+    def test_sending_some_data(self):
+        c = h2.connection.H2Connection()
+        frames = c.send_headers_on_stream(1, self.example_request_headers)
+        frames.append(c.send_data_on_stream(1, b'test', end_stream=True))
+        assert len(frames) == 2
