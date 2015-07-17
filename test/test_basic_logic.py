0ad7b2c7c445208f8f283066fd37e333db4b6dea
Cory Benfield <lukasaoz@gmail.com>
2015-07-17 17:27:10 +0100

    Basic receive headers test

--- test/test_basic_logic.py
+++ test/test_basic_logic.py
@@ -7,6 +7,8 @@ Test the basic logic of the h2 state machines.
 """
 import h2.connection
 
+from hyperframe import frame
+
 
 class TestBasicConnection(object):
     """
@@ -29,3 +31,11 @@ class TestBasicConnection(object):
         frames = c.send_headers_on_stream(1, self.example_request_headers)
         frames.append(c.send_data_on_stream(1, b'test', end_stream=True))
         assert len(frames) == 2
+
+    def test_receive_headers_frame(self):
+        f = frame.HeadersFrame(1)
+        f.data = b'fake headers'
+        f.flags = set(['END_STREAM', 'END_HEADERS'])
+
+        c = h2.connection.H2Connection()
+        assert c.receive_frame(f) is None
