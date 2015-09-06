512da1bb79fb3be0a9bbfd759544b2a8c17709a6
Cory Benfield <lukasaoz@gmail.com>
2015-09-06 13:48:06 +1200

    Define the WindowUpdated event.

--- h2/events.py
+++ h2/events.py
@@ -42,3 +42,16 @@ class DataReceived(object):
     def __init__(self):
         self.stream_id = None
         self.data = None
+
+
+class WindowUpdated(object):
+    """
+    The WindowUpdated event is fired whenever a flow control window changes
+    size. HTTP/2 defines flow control windows for connections and streams: this
+    event fires for both connections and streams. The event carries the ID of
+    the stream to which it applies (set to zero if the window update applies to
+    the connection), and the delta in the window size.
+    """
+    def __init__(self):
+        self.stream_id = None
+        self.delta = None
