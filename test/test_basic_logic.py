c781646e98ea70545015459593ec8b81be9f5fa1
Cory Benfield <lukasaoz@gmail.com>
2015-09-14 15:48:48 +0100

    Test the limits of closing streams.

--- test/test_basic_logic.py
+++ test/test_basic_logic.py
@@ -84,6 +84,36 @@ class TestBasicClient(object):
         assert not events
         assert c.data_to_send == b'\x00\x00\t\x00\x00\x00\x00\x00\x01some data'
 
+    def test_closing_stream_sending_data(self, frame_factory):
+        """
+        We can close a stream with a data frame.
+        """
+        c = h2.connection.H2Connection()
+        c.initiate_connection()
+        c.send_headers(1, self.example_request_headers)
+
+        f = frame_factory.build_data_frame(
+            data=b'some data',
+            flags=['END_STREAM'],
+        )
+
+        # Clear the data, then send some data.
+        c.data_to_send = b''
+        events = c.send_data(1, b'some data', end_stream=True)
+        assert not events
+        assert c.data_to_send == f.serialize()
+
+    def test_cannot_send_on_closed_stream(self):
+        """
+        When we've closed a stream locally, we cannot send further data.
+        """
+        c = h2.connection.H2Connection()
+        c.initiate_connection()
+        c.send_headers(1, self.example_request_headers, end_stream=True)
+
+        with pytest.raises(h2.exceptions.ProtocolError):
+            c.send_data(1, b'some data')
+
     def test_receiving_a_response(self, frame_factory):
         """
         When receiving a response, the ResponseReceived event fires.
