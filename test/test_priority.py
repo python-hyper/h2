46c01fb9fae0cae88626d1cca66a01910034f0b8
Cory Benfield <lukasaoz@gmail.com>
2015-12-14 14:29:34 +0000

    Actual Priority tests.

--- test/test_priority.py
+++ test/test_priority.py
@@ -3,11 +3,10 @@
 test_priority
 ~~~~~~~~~~~~~
 
-Test the priority logic of Hyper-h2. Currently Hyper-h2 has a no-op logic for
-priority (which is to say, it's ignored), but in future we'll try to do
-something reasonable with it.
+Test the priority logic of Hyper-h2.
 """
 import h2.connection
+import h2.events
 import h2.stream
 
 
@@ -15,9 +14,16 @@ class TestPriority(object):
     """
     Basic priority tests.
     """
-    def test_receiving_priority_does_nothing(self, frame_factory):
+    example_request_headers = [
+        (':authority', 'example.com'),
+        (':path', '/'),
+        (':scheme', 'https'),
+        (':method', 'GET'),
+    ]
+
+    def test_receiving_priority_emits_priority_update(self, frame_factory):
         """
-        Receiving a priority frame does nothing.
+        Receiving a priority frame emits a PriorityUpdate event.
         """
         c = h2.connection.H2Connection(client_side=False)
         c.initiate_connection()
@@ -30,5 +36,42 @@ class TestPriority(object):
         )
         events = c.receive_data(f.serialize())
 
-        assert not events
+        assert len(events) == 1
         assert not c.data_to_send()
+
+        event = events[0]
+        assert isinstance(event, h2.events.PriorityUpdate)
+        assert event.stream_id == 1
+        assert event.depends_on == 0
+        assert event.weight == 255
+        assert event.exclusive is False
+
+    def test_headers_with_priority_info(self, frame_factory):
+        """
+        Receiving a HEADERS frame with priority information on it emits a
+        PriorityUpdate event.
+        """
+        c = h2.connection.H2Connection(client_side=False)
+        c.initiate_connection()
+        c.receive_data(frame_factory.preamble())
+        c.clear_outbound_data_buffer()
+
+        f = frame_factory.build_headers_frame(
+            headers=self.example_request_headers,
+            stream_id=3,
+            flags=['PRIORITY'],
+            stream_weight=16,
+            depends_on=1,
+            exclusive=True,
+        )
+        events = c.receive_data(f.serialize())
+
+        assert len(events) == 2
+        assert not c.data_to_send()
+
+        event = events[1]
+        assert isinstance(event, h2.events.PriorityUpdate)
+        assert event.stream_id == 3
+        assert event.depends_on == 1
+        assert event.weight == 16
+        assert event.exclusive is True
