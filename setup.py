424e2311b475ec61816048e85a8657d2da9eccff
Cory Benfield <lukasaoz@gmail.com>
2016-01-08 11:19:49 +0000

    Require at least hyperframe 3.0.1

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
     install_requires=[
-        'hyperframe~=3.0',
+        'hyperframe~=3.0,>=3.0.1',
         'hpack~=2.0',
     ],
     extras_require={
