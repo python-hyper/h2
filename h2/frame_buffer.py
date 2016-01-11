165ac114776fffacbb56ecaf60ea37229f3b6361
Cory Benfield <lukasaoz@gmail.com>
2016-01-11 18:19:37 +0000

    Refactor the FrameBuffer next method.

--- h2/frame_buffer.py
+++ h2/frame_buffer.py
@@ -43,6 +43,27 @@ class FrameBuffer(object):
 
         self.data += data
 
+    def _parse_frame_header(self, data):
+        """
+        Parses the frame header from the data. Either returns a tuple of
+        (frame, length), or throws an exception. The returned frame may be None
+        if the frame is of unknown type.
+        """
+        try:
+            frame, length = Frame.parse_frame_header(data[:9])
+        except UnknownFrameError as e:
+            # Here we do something a bit odd. We want to consume the frame data
+            # as consistently as possible, but we also don't ever want to yield
+            # None. Instead, we make sure that, if there is no frame, we
+            # recurse into ourselves.
+            length = e.length
+            frame = None
+        except ValueError as e:
+            # The frame header is invalid. This is a ProtocolError
+            raise ProtocolError("Invalid frame header received: %s" % str(e))
+
+        return frame, length
+
     def _validate_frame_length(self, length):
         """
         Confirm that the frame is an appropriate length.
@@ -53,41 +74,11 @@ class FrameBuffer(object):
                 (length, self.max_frame_size)
             )
 
-    # The methods below support the iterator protocol.
-    def __iter__(self):
-        return self
-
-    def next(self):  # Python 2
-        # TODO: This method is a *monster*, and desperately needs refactoring
-        # to be cleaner.
-        if len(self.data) < 9:
-            raise StopIteration()
-
-        try:
-            f, length = Frame.parse_frame_header(self.data[:9])
-        except UnknownFrameError as e:
-            # Here we do something a bit odd. We want to consume the frame data
-            # as consistently as possible, but we also don't ever want to yield
-            # None. Instead, we make sure that, if there is no frame, we
-            # recurse into ourselves.
-            length = e.length
-            f = None
-        except ValueError as e:
-            # The frame header is invalid. This is a ProtocolError
-            raise ProtocolError("Invalid frame header received: %s" % str(e))
-
-        if len(self.data) < length + 9:
-            raise StopIteration()
-
-        self._validate_frame_length(length)
-
-        # Don't try to parse the body if we didn't get a frame we know about:
-        # there's nothing we can do with it anyway.
-        if f is not None:
-            f.parse_body(memoryview(self.data[9:9+length]))
-
-        self.data = self.data[9+length:]
-
+    def _update_header_buffer(self, f):
+        """
+        Updates the internal header buffer. Returns a frame that should replace
+        the current one. May throw exceptions if this frame is invalid.
+        """
         # Check if we're in the middle of a headers block. If we are, this
         # frame *must* be a CONTINUATION frame with the same stream ID as the
         # leading HEADERS frame. Anything else is a ProtocolError. If the frame
@@ -121,6 +112,40 @@ class FrameBuffer(object):
             # act like we didn't receive one.
             self._headers_buffer.append(f)
             f = None
+
+        return f
+
+    # The methods below support the iterator protocol.
+    def __iter__(self):
+        return self
+
+    def next(self):  # Python 2
+        # First, check that we have enough data to successfully parse the
+        # next frame header. If not, bail. Otherwise, parse it.
+        if len(self.data) < 9:
+
+            raise StopIteration()
+        f, length = self._parse_frame_header(self.data)
+
+        # Next, check that we have enough length to parse the frame body. If
+        # not, bail, leaving the frame header data in the buffer for next time.
+        if len(self.data) < length + 9:
+            raise StopIteration()
+
+        # Confirm the frame has an appropriate length.
+        self._validate_frame_length(length)
+
+        # Don't try to parse the body if we didn't get a frame we know about:
+        # there's nothing we can do with it anyway.
+        if f is not None:
+            f.parse_body(memoryview(self.data[9:9+length]))
+
+        # At this point, as we know we'll use or discard the entire frame, we
+        # can update the data.
+        self.data = self.data[9+length:]
+
+        # Pass the frame through the header buffer.
+        f = self._update_header_buffer(f)
 
         # If we got a frame we didn't understand or shouldn't yield, rather
         # than return None it'd be better if we just tried to get the next
