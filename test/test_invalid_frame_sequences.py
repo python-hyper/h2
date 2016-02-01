81605085eb9fdcd5c823bc7e4f3fbbd5c65b1352
Cory Benfield <lukasaoz@gmail.com>
2016-02-01 16:05:03 +0000

    Throw ProtocolError for invalid header blocks.

--- test/test_invalid_frame_sequences.py
+++ test/test_invalid_frame_sequences.py
@@ -389,3 +389,27 @@ class TestInvalidFrameSequences(object):
             error_code=h2.errors.PROTOCOL_ERROR
         )
         assert c.data_to_send() == expected_frame.serialize()
+
+    def test_invalid_header_data_protocol_error(self, frame_factory):
+        """
+        If an invalid header block is received, we raise a ProtocolError.
+        """
+        c = h2.connection.H2Connection(client_side=False)
+        c.initiate_connection()
+        c.receive_data(frame_factory.preamble())
+        c.clear_outbound_data_buffer()
+
+        f = frame_factory.build_headers_frame(
+            stream_id=1,
+            headers=self.example_request_headers
+        )
+        f.data = b'\x00\x00\x00\x00'
+
+        with pytest.raises(h2.exceptions.ProtocolError):
+            c.receive_data(f.serialize())
+
+        expected_frame = frame_factory.build_goaway_frame(
+            last_stream_id=0,
+            error_code=h2.errors.PROTOCOL_ERROR
+        )
+        assert c.data_to_send() == expected_frame.serialize()
