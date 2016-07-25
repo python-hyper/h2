42cf2c9bbef7b4f56409ef5adcad30eeaa06935e
Cory Benfield <lukasaoz@gmail.com>
2016-07-25 09:43:46 +0200

    Docstrings are also a thing.

--- h2/config.py
+++ h2/config.py
@@ -17,6 +17,20 @@ class H2Configuration(object):
 
     This object has very little behaviour of its own: it mostly just ensures
     that configuration is self-consistent.
+
+    :param client_side: Whether this object is to be used on the client side of
+        a connection, or on the server side. Affects the logic used by the
+        state machine, the default settings values, the allowable stream IDs,
+        and several other properties. Defaults to ``True``.
+    :type client_side: ``bool``
+
+    :param header_encoding: Controls whether the headers emitted by this object
+        in events are transparently decoded to ``unicode`` strings, and what
+        encoding is used to do that decoding. For historical reasons, this
+        defaults to ``'utf-8'``. To prevent the decoding of headers (that is,
+        to force them to be returned as bytestrings), this can be set to
+        ``False`` or the empty string.
+    :type header_encoding: ``str``, ``False``, or ``None``
     """
     def __init__(self, client_side=True, header_encoding='utf-8'):
         self._client_side = client_side
