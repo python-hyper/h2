c6c90eb34e3aa1d8b254848aaf7019e5b631a07c
Cory Benfield <lukasaoz@gmail.com>
2016-07-25 09:41:25 +0200

    Header encoding cannot be True

--- h2/config.py
+++ h2/config.py
@@ -60,4 +60,6 @@ class H2Configuration(object):
         """
         if not isinstance(value, (bool, str, type(None))):
             raise ValueError("header_encoding must be bool, string, or None")
+        if value is True:
+            raise ValueError("header_encoding cannot be True")
         self._header_encoding = value
