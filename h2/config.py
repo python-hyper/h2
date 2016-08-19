c5e48882ac2e773e958ab2d2b3b691b5dd3011cc
Alex Chan <alex@alexwlchan.net>
2016-08-19 07:18:00 +0100

    Cache the name of the stored attribute

--- h2/config.py
+++ h2/config.py
@@ -14,14 +14,15 @@ class _BooleanConfigOption(object):
     """
     def __init__(self, name):
         self.name = name
+        self.attr_name = '_%s' % self.name
 
     def __get__(self, instance, owner):
-        return getattr(instance, '_%s' % self.name)
+        return getattr(instance, self.attr_name)
 
     def __set__(self, instance, value):
         if not isinstance(value, bool):
             raise ValueError("%s must be a bool" % self.name)
-        setattr(instance, '_%s' % self.name, value)
+        setattr(instance, self.attr_name, value)
 
 
 class H2Configuration(object):
