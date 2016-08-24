a336e4fc632ed252c6bb10f6ab81cad3a0db41d5
Cory Benfield <lukasaoz@gmail.com>
2016-08-24 13:39:16 -0400

    Update tests with new error code representation.

--- test/test_events.py
+++ test/test_events.py
@@ -235,7 +235,8 @@ class TestEventReprs(object):
         e.remote_reset = False
 
         assert repr(e) == (
-            "<StreamReset stream_id:919, error_code:11, remote_reset:False>"
+            "<StreamReset stream_id:919, "
+            "error_code:ErrorCodes.ENHANCE_YOUR_CALM, remote_reset:False>"
         )
 
     def test_pushedstreamreceived_repr(self):
@@ -300,8 +301,8 @@ class TestEventReprs(object):
         e.additional_data = additional_data
 
         assert repr(e) == (
-            "<ConnectionTerminated error_code:12, last_stream_id:33, "
-            "additional_data:%s>" % data_repr
+            "<ConnectionTerminated error_code:ErrorCodes.INADEQUATE_SECURITY, "
+            "last_stream_id:33, additional_data:%s>" % data_repr
         )
 
     def test_alternativeserviceavailable_repr(self):
