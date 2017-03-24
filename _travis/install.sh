02353bb83d0193c3940980d8e5f3519899f75730
Cory Benfield <lukasaoz@gmail.com>
2017-03-24 16:31:32 +0000

    Use --raw-output to avoid extra quotes

--- _travis/install.sh
+++ _travis/install.sh
@@ -11,7 +11,7 @@ if [ $TOXENV = "h2spec" ]; then
     # Github API for it, and then parsing the JSON for the appropriate kind of
     # binary. Happily, the binary is always called "h2spec" so we don't need
     # even more shenanigans to get this to work.
-    TARBALL=$(curl -s https://api.github.com/repos/summerwind/h2spec/releases/latest | jq '.assets[] | .browser_download_url | select(endswith("linux_amd64.tar.gz"))')
+    TARBALL=$(curl -s https://api.github.com/repos/summerwind/h2spec/releases/latest | jq --raw-output '.assets[] | .browser_download_url | select(endswith("linux_amd64.tar.gz"))')
     curl -LO "$TARBALL"
     tar xvf $TARBALL
     mv h2spec /usr/local/bin
