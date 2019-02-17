aa713d4f0a8d15819c64a6b72e8a99283e868c98
Andrew Huang <k.andrew.huang@gmail.com>
2019-02-17 11:06:42 -0500

    Add test to exercise incr conditional in sync_state_change wrapper

--- test/test_concurrent_stream_open.py
+++ test/test_concurrent_stream_open.py
@@ -5,12 +5,9 @@ test_flow_control
 
 Tests of the flow control management in h2
 """
-import pytest
-import time
 import logging
+import time
 
-from hypothesis import given
-from hypothesis.strategies import integers
 
 import h2.config
 import h2.connection
@@ -18,9 +15,10 @@ import h2.errors
 import h2.events
 import h2.exceptions
 import h2.settings
+from h2.stream import H2Stream, sync_state_change
 
 
-class TestConcurrentStreamOpenPerformance(object):
+class TestConcurrentStreamOpen(object):
     """
     Tests the performance of concurrently opening streams
     """
@@ -31,8 +29,28 @@ class TestConcurrentStreamOpenPerformance(object):
         (':method', 'GET'),
     ]
     server_config = h2.config.H2Configuration(client_side=False)
+    client_config = h2.config.H2Configuration(client_side=True)
 
     DEFAULT_FLOW_WINDOW = 65535
+
+    def test_sync_state_change_incr_conditional(self, frame_factory):
+
+        @sync_state_change
+        def wrap_send_headers(self, *args, **kwargs):
+            return self.send_headers(*args, **kwargs)
+
+        def dummy_callback(*args, **kwargs):
+            pass
+
+        c = h2.connection.H2Connection()
+        s = H2Stream(1, self.client_config, self.DEFAULT_FLOW_WINDOW,
+                     self.DEFAULT_FLOW_WINDOW, dummy_callback,
+                     dummy_callback)
+        s.max_outbound_frame_size = 65536
+
+        wrap_send_headers(s, self.example_request_headers,
+                          c.encoder, end_stream=False)
+        assert s.open
 
     def test_concurrent_stream_open_performance(self, frame_factory):
         """
@@ -45,20 +63,22 @@ class TestConcurrentStreamOpenPerformance(object):
         c.config.logger.setLevel(logging.DEBUG)
         start = time.time()
         for i in range(num_concurrent_streams):
-            c.send_headers(1 + (2 * i), self.example_request_headers, end_stream=False)
+            c.send_headers(
+                1 + (2 * i), self.example_request_headers, end_stream=False)
             c.clear_outbound_data_buffer()
         end = time.time()
 
         with_debug_logging = end-start
-        
+
         c = h2.connection.H2Connection()
         c.initiate_connection()
         start = time.time()
         for i in range(num_concurrent_streams):
-            c.send_headers(1 + (2 * i), self.example_request_headers, end_stream=False)
+            c.send_headers(
+                1 + (2 * i), self.example_request_headers, end_stream=False)
             c.clear_outbound_data_buffer()
         end = time.time()
-        
+
         run_time = end - start
         assert run_time < 5
         assert with_debug_logging > run_time
